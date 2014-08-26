dcf59c37a69b106b5a0588476d95bbfb70ceb0a6
Shuvendu Lahiri <shuvendu@microsoft.com>
2014-08-26 11:44:24 -0700

    adding a stub file for build to compile

---
+++ Dependency/source/AbstractedTaint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Boogie;
+
+namespace Dependency
+{
+    public class AbstractedTaint
+    {
+        public abstract class SuperBlock
+        {
+            public Block StartBlock, EndBlock;
+            public List<Block> AllBlocks;
+        }
+
+
+        internal static void CreateAbstractedTaintProgram(Microsoft.Boogie.Program program, Dictionary<Microsoft.Boogie.Procedure, Dependencies> allDeps, Dictionary<Microsoft.Boogie.Block, Dependencies> blockDeps, Dictionary<Microsoft.Boogie.Procedure, List<Microsoft.Boogie.Block>> taintedBlocks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
