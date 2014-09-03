cd47a2da87d4148feea844f61a99ff5704c26045
Nimrod Sebban Partush <nimrodpar@gmail.com>
2014-09-02 17:04:26 -0700

    added demo

---
+++ Dependency/Test/regression/TestDemo/source/demo.c
@@ -0,0 +1,44 @@
+int main() {
+	int a,b,c,d;
+	a = before(a);
+	b = tainted(b);
+	if (b < 0)
+		b = 0;
+	c = after(b);
+	d = alias(d);
+	return 0;
+}
+
+int leaf(int x) {
+	int y = x + 1;
+	return y;
+}
+
+int before(int x) {
+	int i, res = 0;
+	for (i = 0; i < 80; ++i)
+		res += leaf(x);
+	return res;
+}
+
+int tainted(int x) {
+	int y;
+	int *p = &y;
+	y++; // line change
+	*p++;
+	leaf(y); 
+	return y; 
+}
+
+int after(int x) {
+	if (x < 0)
+		return x;
+	return 0;
+}
+
+int alias(int x) {
+	int y;
+	int *q = &y;
+	*q = 2;
+	return y;
+}
