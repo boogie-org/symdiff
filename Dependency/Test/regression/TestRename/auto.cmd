ffb4111ccbafb4428439fd41329cf11bed065826
Alex Gyori <t-algy@microsoft.com>
2015-06-20 19:14:21 +0100

    updated auto to use the changedLines flag

--- Dependency/Test/regression/TestRename/auto.cmd
+++ Dependency/Test/regression/TestRename/auto.cmd
@@ -1,5 +1,5 @@
 call run_symdiff_c.cmd v1 v2 /nocpp /rvt /nohtml /genBplsOnly /analyzeChangedCallersOnly /stripAbsolutePathsInBpl
 
-call run_symdiff_bpl v1 v2 /rvt /opts:" -usemutual -asserts -checkEquivWithDependencies -freeContracts "
+call run_symdiff_bpl v1 v2 /rvt /opts:" -usemutual -asserts -checkEquivWithDependencies -freeContracts " /changedLines
 
 call Dependency.exe _v2.bpl /taint:v2\changed_lines.txt /prune /dacMerged:mergedProgSingle_inferred.bpl
