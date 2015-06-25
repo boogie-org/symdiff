d3d4498cc4f22705f6eb8c9aebe47f4b210a8e64
Alex Gyori <t-algy@microsoft.com>
2015-06-25 12:01:21 -0700

    updated auto to play with smack

--- Dependency/Test/regression/TestRename/auto.cmd
+++ Dependency/Test/regression/TestRename/auto.cmd
@@ -1,4 +1,10 @@
-call run_symdiff_c.cmd v1 v2 /nocpp /rvt /nohtml /genBplsOnly /analyzeChangedCallersOnly /stripAbsolutePathsInBpl
+## call run_symdiff_c.cmd v1 v2 /nocpp /rvt /nohtml /genBplsOnly /analyzeChangedCallersOnly /stripAbsolutePathsInBpl
+
+..\..\..\..\SmackProcessing\bin\Debug\SmackProcessing.exe smack_v1.bpl
+copy smack_v1_unsmacked.bpl v1.bpl
+
+..\..\..\..\SmackProcessing\bin\Debug\SmackProcessing.exe smack_v2.bpl
+copy smack_v2_unsmacked.bpl v2.bpl
 
 call run_symdiff_bpl v1 v2 /rvt /opts:" -usemutual -asserts -checkEquivWithDependencies -freeContracts " /changedLines
 
