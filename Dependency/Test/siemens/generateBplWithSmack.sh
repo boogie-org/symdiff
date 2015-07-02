b755843c8690978ee2603f0670b0e825cce42fe7
Alex Gyori <t-algy@microsoft.com>
2015-07-02 01:51:06 +0100

    added necessary script, patches, and makefile to run smack on siemens

---
+++ Dependency/Test/siemens/generateBplWithSmack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CRNT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+
+for file in `find $CRNT_DIR -name '*'`; do dos2unix $file; done
+
+for proj in $(ls $CRNT_DIR); do
+    cd $CRNT_DIR
+    if [ -d $CRNT_DIR/$proj ]
+    then
+	cd $CRNT_DIR/$proj
+	cd source
+	patch -N < ../source/original.patch
+	make -f smackMakefile clean all &> /dev/null
+	#diff -u orig.$proj.c $proj.c  > original.patch
+	cd -
+
+	for dir in $(ls -d v*); do
+	    if [ -d $dir ]
+	    then
+		cd $dir
+		pwd
+		cp ../source/smackMakefile .
+		patch -N < ../source/original.patch
+		make -f smackMakefile clean all &> /dev/null
+		cd $CRNT_DIR/$proj
+	    fi
+	done
+
+    fi
+done
+
