5952807ae3c74fba19e678974319ac5988c76885
Shuvendu Lahiri <shuvendu@microsoft.com>
2015-08-12 19:45:16 -0700

    adding missing inliner.cs

---
+++ Dependency/source/Inliner.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Boogie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency.source
+{
+    class DependencyInliner
+    {
+        private Program program;
+        private int InlineDepth;
+
+        public DependencyInliner(Program program, int InlineDepth)
+        {
+            this.program = program;
+            this.InlineDepth = InlineDepth;
+        }
+        internal Program InlineImplementations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
