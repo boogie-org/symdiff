270d2f39df99daf28b3ce77f0812c5758506c90b
Alex Gyori <t-algy@microsoft.com>
2015-08-16 16:22:39 -0700

    improved finding changed procedures

--- Dependency/source/Utils.cs
+++ Dependency/source/Utils.cs
@@ -314,7 +314,8 @@ namespace Dependency
             {
                 foreach (var changesPerProc in changesPerFile.GroupBy(t => t.Item2))
                 {
-                    foreach (var impl in program.Implementations.Where(i => i.Proc.Name.StartsWith(changesPerProc.Key))) // dealing with loops which are procs with name <orig_proc>_loop_head etc.
+                    // Maybe in the future add an attribute to extracted procedures containing the name of the original procedure
+                    foreach (var impl in program.Implementations.Where(i => i.Proc.Name.StartsWith(changesPerProc.Key + "_loop_"))) // dealing with loops which are procs with name <orig_proc>_loop_head etc.
                     {
                         if (changesPerProc.FirstOrDefault(t => t.Item3 == Utils.AttributeUtils.WholeProcChangeAttributeVal) == null)
                             foreach (var procChange in changesPerProc)
