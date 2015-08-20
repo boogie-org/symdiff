bd4a8633e76baf087c763ddcac24b200fb8400d8
Shuvendu Lahiri <shuvendu@microsoft.com>
2015-08-20 16:04:40 -0700

    more info on inference for inlineDepth

--- Dependency/Test/regression/TestInlineBeforeDAC/auto.cmd
+++ Dependency/Test/regression/TestInlineBeforeDAC/auto.cmd
@@ -12,3 +12,14 @@ REM Inferred contracts: inlineDepth = 0, 5/15 inferred
 REM Inferred contracts: inlineDepth = 1, 13/15 inferred
 REM Inferred contracts: inlineDepth = 2, 13/15 inferred
 
+//The only removals are postconditions of A, as expected
+
+REM When using boogie directly on inlineDepth = 2, we don't infer the following
+REM auto.cmd 
+REM boogie /contractInfer mergedProgSingle_preInferred.bpl /trace /inlineDepth:2
+REM requires {:DAC_LE _v1.y, _v2.y} _houdini_1 ==> _v1.y <= _v2.y;
+REM requires {:DAC_LE _v2.y, _v1.y} _houdini_2 ==> _v2.y <= _v1.y;
+
+
+
+
