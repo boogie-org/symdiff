c0623c835f2c17841da262b54cb344981a503f8a
Shuvendu Lahiri <shuvendu@microsoft.com>
2015-08-20 15:40:50 -0700

    minor fix in documentation

--- Dependency/Test/regression/TestInlineBeforeDAC/auto.cmd
+++ Dependency/Test/regression/TestInlineBeforeDAC/auto.cmd
@@ -5,5 +5,10 @@ REM Example to test inlining and DAC
 
 @echo on
 
-run_symdiff_bpl.cmd v0 v1 /rvt /coarseDiff /opts:" -usemutual -checkEquivWithDependencies -freeContracts  -inlineDepthInferContracts:1 " /changedLines 
+run_symdiff_bpl.cmd v0 v1 /rvt /coarseDiff /opts:" -usemutual -checkEquivWithDependencies -freeContracts  -inlineDepthInferContracts:2 " /changedLines 
+
+REM Changing the inlineDepthInferContracts from {0, 1, 2}
+REM Inferred contracts: inlineDepth = 0, 5/15 inferred
+REM Inferred contracts: inlineDepth = 1, 13/15 inferred
+REM Inferred contracts: inlineDepth = 2, 13/15 inferred
 
